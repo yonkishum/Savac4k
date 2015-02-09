485741e4199847b6159052f3fc97b12cf5342d79
yonkishum <yonkishum@gmail.com>
2015-02-08 21:10:02 -0430

    Modulo de clases completado

--- app/src/main/java/yan/samux/ikaros/com/savac4k/Activity_Learn.java
+++ app/src/main/java/yan/samux/ikaros/com/savac4k/Activity_Learn.java
@@ -1,39 +1,77 @@
 package yan.samux.ikaros.com.savac4k;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class Activity_Learn extends ActionBarActivity {
+
+	ListView mlista;
+	String[] nClass;
+	int[] iClass = {
+			R.drawable.ic_learning_basic,
+			R.drawable.ic_learning_numbers,
+			R.drawable.ic_learning_animals,
+			R.drawable.ic_learning_object,
+			R.drawable.ic_learning_country,
+			R.drawable.ic_learning_gesture,
+			R.drawable.ic_learning_run
+	};
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_activity__learn);
+
+		Resources resources = getResources();
+
+		nClass = resources.getStringArray(R.array.clases);
+		//iClass = resources.getIntArray(R.array.pic_clases);
+
+		mlista = (ListView) findViewById(R.id.list_class);
+
+		classAdapter adapter = new classAdapter(this, nClass, iClass);
+
+		mlista.setAdapter(adapter);
+
+	}
+}
+
+class classAdapter extends ArrayAdapter<String> {
+
+	Context context;
+	int[] ico_class;
+	String[] name_class;
+
+	classAdapter(Context c, String[] nmClass, int[] imClass) {
+		super(c, R.layout.list_class_room, R.id.classname, nmClass);
+		this.context = c;
+		this.name_class = nmClass;
+		this.ico_class = imClass;
 	}
 
-
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_activity__learn, menu);
-		return true;
-	}
+	public View getView(int position, View convertView, ViewGroup parent) {
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View row = inflater.inflate(R.layout.list_class_room, parent, false);
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
+		ImageView icotype = (ImageView) row.findViewById(R.id.classicons);
+		TextView nametype = (TextView) row.findViewById(R.id.classname);
 
-		return super.onOptionsItemSelected(item);
+		icotype.setImageResource(ico_class[position]);
+		nametype.setText(name_class[position]);
+
+		return row;
 	}
 }
