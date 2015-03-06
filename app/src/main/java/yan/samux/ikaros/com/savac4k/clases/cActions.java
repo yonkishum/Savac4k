41cf9e1ad8155e7efff2396261243e1b55f3cd3a
Yan <Yonkishum@gmail.com>
2015-03-06 09:29:55 -0430

    Se añade los metodos para hacer funcionar el JSONarrayrequest y mostrar el adaptador en pantalla

--- app/src/main/java/yan/samux/ikaros/com/savac4k/clases/cActions.java
+++ app/src/main/java/yan/samux/ikaros/com/savac4k/clases/cActions.java
@@ -1,17 +1,108 @@
 package yan.samux.ikaros.com.savac4k.clases;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.ListView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import yan.samux.ikaros.com.savac4k.CustomListAdapterClases;
 import yan.samux.ikaros.com.savac4k.R;
+import yan.samux.ikaros.com.savac4k.app.AppControllerClass;
+import yan.samux.ikaros.com.savac4k.models.Acciones;
 
 public class cActions extends ActionBarActivity {
+
+    private static final String TAG = cActions.class.getSimpleName();
+
+    private static final String url = "http://192.168.0.107/savac4kcon/index.php";
+    private ProgressDialog pDialog;
+    private List<Acciones> accionesList = new ArrayList<Acciones>();
+    private CustomListAdapterClases adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c_actions);
+
+
+        ListView listView = (ListView) findViewById(R.id.csAcciones);
+
+        adapter = new CustomListAdapterClases(this, accionesList);
+
+        listView.setAdapter(adapter);
+
+        pDialog = new ProgressDialog(this);
+
+        pDialog.setMessage("Loading...");
+
+        pDialog.show();
+
+        JsonArrayRequest movieReq = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        hidePDialog();
+
+                        // Parsing json
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+
+                                JSONObject obj = response.getJSONObject(i);
+                                Acciones acciones = new Acciones();
+                                acciones.setPalabra(obj.getString("_nombreacciones"));
+                                acciones.setThumbnailUrl(obj.getString("_imagen"));
+
+                                accionesList.add(acciones);
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                        // notifying list adapter about data changes
+                        // so that it renders the list view with updated data
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hidePDialog();
+
+            }
+        });
+
+        // Adding request to request queue
+        AppControllerClass.getInstance().addToRequestQueue(movieReq);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        hidePDialog();
+    }
+
+    private void hidePDialog() {
+        if (pDialog != null) {
+            pDialog.dismiss();
+            pDialog = null;
+        }
     }
 }
